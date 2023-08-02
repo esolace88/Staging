724a456d9537863b1259b22fadb8b839cb6d2c02
Eric Solis <63070647+esolace88@users.noreply.github.com>
2023-08-02 09:15:54 -0500

    Update vault_provider.tf

--- Test/vault_provider.tf
+++ Test/vault_provider.tf
@@ -11,6 +11,22 @@
  }
 
  provider "azurerm" {
+   version = "~>2.0"
+   subscription_id = var_subcription_id
+   client_id = var.sp_id
+   client_secret = var.sp_secret
+   tenant_id = var.tenant_id
    features {}
-   skip_provider_registration = true
  }
+
+
+provider "azurerm" {
+
+    version = "~>2.0"
+        subscription_id = ""
+        client_id = "sp appId"
+        client_secret = "sp password"
+        tenant_id = "sp tenant"
+        features {}
+
+}
